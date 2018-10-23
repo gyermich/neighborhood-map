8f6453816c9d1b731fd462b3d4fecdbf75db9705
galina <galina@energysavvy.com>
2018-10-23 13:55:18 -0700

    use Foresquare api to get sights

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+
+  state = {
+    sights: []
+  }
+
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
+      center: {lat: 35.652832, lng: 139.839478},
+      zoom: 10
+    })
+
+    this.state.sights.forEach(sight => {
+      new window.google.maps.Marker({
+          position: {
+            lat: sight.venue.location.lat,
+            lng: sight.venue.location.lng},
+          map: map,
+          title: sight.venue.name
+        })
+    })
   }
 
   loadScript = (url) => {
@@ -23,8 +39,29 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+  getSights = () => {
+    const endpoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const params = {
+      client_id: '',
+      client_secret: '',
+      query: 'sights',
+      near: 'Tokyo',
+      v: '20181023'
+    }
+
+    axios.get(endpoint + new URLSearchParams(params))
+      .then(response => {
+        this.setState({
+          sights: response.data.response.groups[0].items
+        },     this.loadMap())
+      })
+      .catch(error => {
+        console.log("ERROR: ", error)
+      })
+  }
+
   componentDidMount() {
-    this.loadMap()
+    this.getSights()
   }
 
   render() {
